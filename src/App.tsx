87ae4f5a472de864731d1b2053085cd211c0e0d5
Oleksii <mysterious240@gmail.com>
2022-05-27 16:23:41 +0300

    changed folders path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import PageInfo from './components/PageInfo'
+import PageInfo from './components/pageInfo/PageInfo'
 import './App.css';
 
-import Home from './components/Home';
+import Home from './components/home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
